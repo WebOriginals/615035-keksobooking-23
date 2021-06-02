430c88fb508f5bc5fa4858825986003cdeab96e3
KidsGo <70588360+WebOriginals@users.noreply.github.com>
2021-06-02 23:03:32 +0300

    Исправил все ошибки из линтера

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
 // источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 const randomNumber = function getRandomIntInclusive(min, max) {
-
+   
   if( min > max ){
     return 'минимальное число не может быть больше максимального';
   }else if( min === max ){
-    return 'Вы ввели минимальное число равное максимальному  ' + Math.floor(Math.random() * (max - min + 1)) + min;
+    return 'Вы ввели минимальное число равное максимальному  ${ Math.floor(Math.random() * (max - min + 1)) + min }';
   }else {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-}
+};
 
 
 const randomFloatingPointNumber = function getRandomIntInclusiveFixedPoint(min, max , numberAfterPoint) {
-  let randomNumber = Math.random() * (max - min) + min;
+  const randomNumberFormula = Math.random() * (max - min) + min;
   if( min > max ){
-    return 'минимальное число не может быть больше максимального ' +  +randomNumber.toFixed(numberAfterPoint);
+    return 'минимальное число не может быть больше максимального ${randomNumberFormula.toFixed(numberAfterPoint)}';
   }else if( min === max ){
 
-    return 'Вы ввели минимальное число равное максимальному  ' +  +randomNumber.toFixed(numberAfterPoint);
+    return 'Вы ввели минимальное число равное максимальному ${randomNumberFormula.toFixed(numberAfterPoint)}';
   }else {
-    return +randomNumber.toFixed(numberAfterPoint);
+    return +randomNumberFormula.toFixed(numberAfterPoint);
   }
-}
+};
+
+randomNumber(1, 5);
+randomFloatingPointNumber(1, 5, 1)
