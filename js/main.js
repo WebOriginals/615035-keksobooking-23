b2cd48eb4315ac2815a458dfaa3caeb54292f6cd
KidsGo <70588360+WebOriginals@users.noreply.github.com>
2021-06-02 20:41:01 +0300

    создал функцию показывающую рандомное число с плавающей точкой

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
 // источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomIntInclusive(min, max) {
+let randomNumber = function getRandomIntInclusive(min, max) {
 
   if( min > max ){
     return "минимальное число не может быть больше максимального";
   }else if( min === max ){
     return "Вы ввели минимальное число равное максимальному  " + Math.floor(Math.random() * (max - min + 1)) + min;
-  }else{
+  }else {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
 
-console.log(getRandomIntInclusive(5,5));
-console.log(getRandomIntInclusive(12,10));
-console.log(getRandomIntInclusive(1,4));
+
+let randomFloatingPointNumber = function getRandomIntInclusiveFixedPoint(min, max , numberAfterPoint) {
+  let randomNumber = Math.random() * (max - min) + min;
+  if( min > max ){
+    return "минимальное число не может быть больше максимального " +  +randomNumber.toFixed(numberAfterPoint);
+  }else if( min === max ){
+
+    return "Вы ввели минимальное число равное максимальному  " +  +randomNumber.toFixed(numberAfterPoint);
+  }else {
+    return +randomNumber.toFixed(numberAfterPoint);
+  }
+}
+
+console.log(randomFloatingPointNumber(1, 5,1));
+console.log(randomFloatingPointNumber(1, 1,2));
+console.log(randomFloatingPointNumber(5, 3,0));
+
 
 
 
