5fffa9386e0185ef33a034a3bf2495ef246c2cbb
KidsGo <70588360+WebOriginals@users.noreply.github.com>
2021-06-02 23:13:49 +0300

    исправил проблему с кавычками

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const randomNumber = function getRandomIntInclusive(min, max) {
   if( min > max ){
     return 'минимальное число не может быть больше максимального';
   }else if( min === max ){
-    return 'Вы ввели минимальное число равное максимальному  ${ Math.floor(Math.random() * (max - min + 1)) + min }';
+    return `Вы ввели минимальное число равное максимальному  ${ Math.floor(Math.random() * (max - min + 1)) + min }`;
   }else {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
@@ -14,14 +14,14 @@ const randomNumber = function getRandomIntInclusive(min, max) {
 const randomFloatingPointNumber = function getRandomIntInclusiveFixedPoint(min, max , numberAfterPoint) {
   const randomNumberFormula = Math.random() * (max - min) + min;
   if( min > max ){
-    return 'минимальное число не может быть больше максимального ${randomNumberFormula.toFixed(numberAfterPoint)}';
+    return `минимальное число не может быть больше максимального ${randomNumberFormula.toFixed(numberAfterPoint)}`;
   }else if( min === max ){
 
-    return 'Вы ввели минимальное число равное максимальному ${randomNumberFormula.toFixed(numberAfterPoint)}';
+    return `Вы ввели минимальное число равное максимальному ${randomNumberFormula.toFixed(numberAfterPoint)}`;
   }else {
     return +randomNumberFormula.toFixed(numberAfterPoint);
   }
 };
 
 randomNumber(1, 5);
-randomFloatingPointNumber(1, 5, 1)
+randomFloatingPointNumber(1, 5, 1);
