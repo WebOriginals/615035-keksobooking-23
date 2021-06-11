662edb2339c54f21f2bcfa7f94018274b34ecadf
KidsGo <70588360+WebOriginals@users.noreply.github.com>
2021-06-11 09:27:52 +0300

    третие правки по дз

--- js/main.js
+++ js/main.js
@@ -85,28 +85,28 @@ const SIMILAR_OBJECTS_COUNT = 10;
 const createPlace = () => {
   const randomAvatarIndex = randomNumber(1, 10);
   const randomNumberImg = randomAvatarIndex === 10 ? randomAvatarIndex : `0  ${randomAvatarIndex}`;
-  return {
-    author: {
-      avatar: 'img/avatars/user' + randomNumberImg + '.png',
-    },
-    offer: {
-      title: getСalculatingRandomNumber(TITLE),
-      address: getСalculatingRandomNumber(ADDRESS),
-      price: getСalculatingRandomNumber(PRICE),
-      type: getСalculatingRandomNumber(TYPE),
-      rooms: getСalculatingRandomNumber(ROOMS),
-      guests: getСalculatingRandomNumber(GUESTS),
-      checkin: getСalculatingRandomNumber(CHECKIN),
-      checkout: getСalculatingRandomNumber(CHECKOUT),
-      features: getСalculatingRandomNumber(FEATURES),
-      description: getСalculatingRandomNumber(DESCRIPTION),
-      photos: getСalculatingRandomNumber(PHOTOS),
-    },
-    location: {
-      lat: randomFloatingPointNumber(35.65000, 35.70000, 5),
-      lng: randomFloatingPointNumber(139.70000, 139.80000, 5),
-    }
-  };
+      return {
+        author: {
+        avatar: 'img/avatars/user' + randomNumberImg + '.png',
+        },
+        offer: {
+        title: getСalculatingRandomNumber(TITLE),
+        address: getСalculatingRandomNumber(ADDRESS),
+        price: getСalculatingRandomNumber(PRICE),
+        type: getСalculatingRandomNumber(TYPE),
+        rooms: getСalculatingRandomNumber(ROOMS),
+        guests: getСalculatingRandomNumber(GUESTS),
+        checkin: getСalculatingRandomNumber(CHECKIN),
+        checkout: getСalculatingRandomNumber(CHECKOUT),
+        features: getСalculatingRandomNumber(FEATURES),
+        description: getСalculatingRandomNumber(DESCRIPTION),
+        photos: getСalculatingRandomNumber(PHOTOS),
+        },
+        location: {
+        lat: randomFloatingPointNumber(35.65000, 35.70000, 5),
+        lng: randomFloatingPointNumber(139.70000, 139.80000, 5),
+        }
+      };
 };
 const similarObjects = new Array(SIMILAR_OBJECTS_COUNT).fill(null).map(() => createPlace());
-console.log(similarObjects);
+similarObjects;
