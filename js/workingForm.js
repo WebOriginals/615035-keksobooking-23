2318e8cfbf77c9fd206bd0bbf3c917ad3769eba2
KidsGo <70588360+WebOriginals@users.noreply.github.com>
2021-06-23 18:23:24 +0300

    создал функцию активации и деактивации форм

---
+++ js/workingForm.js
@@ -0,0 +1,19 @@
+const forma = document.querySelector('.ad-form');
+const fieldsets = forma.querySelectorAll('fieldset');
+
+const causeDeactivatingForm = function () {
+  forma.classList.add('ad-form--disabled');
+  for (let i = 0; i < fieldsets.length; i++) {
+    fieldsets[i].setAttribute("disabled", "disabled");
+  }
+}
+
+const activateForm = function () {
+  forma.classList.remove('ad-form--disabled');
+  for (let i = 0; i < fieldsets.length; i++) {
+    fieldsets[i].removeAttribute("disabled", "disabled");
+  }
+}
+
+causeDeactivatingForm();
+activateForm();
