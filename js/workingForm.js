3e212c008e49cdffac662ab7ae5945e18aada6bc
KidsGo <70588360+WebOriginals@users.noreply.github.com>
2021-06-23 18:37:23 +0300

    внес правки

--- js/workingForm.js
+++ js/workingForm.js
@@ -4,16 +4,16 @@ const fieldsets = forma.querySelectorAll('fieldset');
 const causeDeactivatingForm = function () {
   forma.classList.add('ad-form--disabled');
   for (let i = 0; i < fieldsets.length; i++) {
-    fieldsets[i].setAttribute("disabled", "disabled");
+    fieldsets[i].setAttribute('disabled', 'disabled');
   }
-}
+};
 
 const activateForm = function () {
   forma.classList.remove('ad-form--disabled');
   for (let i = 0; i < fieldsets.length; i++) {
-    fieldsets[i].removeAttribute("disabled", "disabled");
+    fieldsets[i].removeAttribute('disabled', 'disabled');
   }
-}
+};
 
 causeDeactivatingForm();
 activateForm();
